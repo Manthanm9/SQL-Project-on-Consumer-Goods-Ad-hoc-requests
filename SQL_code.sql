b6557ad9d2a04de587ad1983c1307c02d620a5d9
Manthan Mehar <114760286+Manthanm9@users.noreply.github.com>
2023-01-23 15:29:39 +0530

    Update SQL_code.sql

--- SQL_code.sql
+++ SQL_code.sql
@@ -1,12 +1,19 @@
-#CODEBASICS_SQL_PROJECT_CHALLENGE
+/*CODEBASICS SQL PROJECT CHALLENGE
+  GENERATING INSIGHTS ON CONSUMER GOODS AD-HOC REQUEST FROM ATLIQ HARDWARE*/
 
-#TASK 1
+
+/*REQUEST 1
+Provide the list of markets in which customer "Atliq Exclusive" operates its
+business in the APAC region.*/
+
 SELECT
     DISTINCT market FROM  dim_customer
 WHERE region = 'APAC' AND customer = "Atliq Exclusive";
 
 
-#TASK 2
+/*REQUEST 2
+What is the percentage of unique product increase in 2021 vs. 2020?*/
+
 WITH unique_products AS (
     SELECT 
         fiscal_year, 
@@ -29,14 +36,20 @@ WHERE
     AND up_2021.fiscal_year = 2021;
    
    
-#TASK 3
+/*REQUEST 3
+Provide a report with all the unique product counts for each segment and
+sort them in descending order of product counts.*/
+
 SELECT segment, count(DISTINCT product_code) AS product_count
 FROM dim_product
 GROUP BY segment
 ORDER BY product_count DESC;
 
 
-#TASK 4
+/*REQUEST 4
+Which segment had the most increase in unique products in
+2021 vs 2020?*/
+
 WITH temp_table AS (
     SELECT 
         p.segment,
@@ -66,7 +79,9 @@ ORDER BY
     difference DESC;
 
 
-#TASK 5
+/* REQUEST 5
+Get the products that have the highest and lowest manufacturing costs.*/
+
 SELECT m.product_code, concat(product," (",variant,")") AS product, cost_year,manufacturing_cost
 FROM fact_manufacturing_cost m
 JOIN dim_product p ON m.product_code = p.product_code
@@ -78,7 +93,11 @@ manufacturing_cost =
 ORDER BY manufacturing_cost DESC;
 
 
-#TASK 6
+/*REQUEST 6
+Generate a report which contains the top 5 customers who received an
+average high pre_invoice_discount_pct for the fiscal year 2021 and in the
+Indian market.*/
+
 SELECT c.customer_code, c.customer, round(AVG(pre_invoice_discount_pct),4) AS average_discount_percentage
 FROM fact_pre_invoice_deductions d
 JOIN dim_customer c ON d.customer_code = c.customer_code
@@ -88,7 +107,11 @@ ORDER BY average_discount_percentage DESC
 LIMIT 5;
 
 
-#TASK 7
+/* REQUEST 7
+Get the complete report of the Gross sales amount for the customer “Atliq
+Exclusive” for each month. This analysis helps to get an idea of low and
+high-performing months and take strategic decisions.*/
+
 WITH temp_table AS (
     SELECT customer,
     monthname(date) AS months ,
@@ -105,7 +128,9 @@ GROUP BY year,months
 ORDER BY year,month_number;
 
 
-#TASK 8
+/* REQUEST 8
+In which quarter of 2020, got the maximum total_sold_quantity?*/
+
 WITH temp_table AS (
   SELECT date,month(date_add(date,interval 4 month)) AS period, fiscal_year,sold_quantity 
 FROM fact_sales_monthly
@@ -121,7 +146,10 @@ GROUP BY quarter
 ORDER BY total_sold_quanity_in_millions DESC ;
 
 
-#TASK 9
+/* REQUEST 9
+Which channel helped to bring more gross sales in the fiscal year 2021
+and the percentage of contribution?*/
+
 WITH temp_table AS (
       SELECT c.channel,sum(s.sold_quantity * g.gross_price) AS total_sales
   FROM
@@ -139,7 +167,10 @@ SELECT
 FROM temp_table ;
 
 
-#TASK 10
+/* REQUEST 10
+Get the Top 3 products in each division that have a high
+total_sold_quantity in the fiscal_year 2021?*/
+
 WITH temp_table AS (
     select division, s.product_code, concat(p.product,"(",p.variant,")") AS product , sum(sold_quantity) AS total_sold_quantity,
     rank() OVER (partition by division order by sum(sold_quantity) desc) AS rank_order
